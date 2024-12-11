108f5943e080d3b8f4dc4ed826007bf83f007a79
Agas Triawan <agastriawan@gmail.com>
2024-12-11 17:32:00 +0700

    Pertemuan 9

---
+++ Data/Fruits.js
@@ -0,0 +1,9 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+const fruits = ["Leci", "Anggur", "Semangka"];
+
+// TODO 2: export variable fruits
+module.exports = fruits;
